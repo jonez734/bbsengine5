2cd722d32c61533a7efa809ca0cf785a22ff637b
Jeff MacDonald <jam@zoidtechnologies.com>
2021-01-28 19:22:59 -0500

    bbsengine5/sql/mantra.sql: commented out engine.__mantra; mantras are now part of the node table.

--- sql/mantra.sql
+++ sql/mantra.sql
@@ -1,21 +1,23 @@
 \echo mantra.sql
-CREATE TABLE engine.__mantra (
-    id serial unique not null primary key,
-    description text,
-    author text,
-    reference text,
-    datecreated timestamp with time zone,
-    createdbyid integer constraint fk_mantra_postedbyid references engine.__member(id) on update cascade on delete set null,
-    dateupdated timestamp with time zone,
-    updatedbyid integer constraint fk_mantra_modifiedbyid references engine.__member(id) on update cascade on delete set null
+--CREATE TABLE engine.__mantra (
+--    id serial unique not null primary key,
+--    description text,
+--    author text,
+--    reference text,
+--    datecreated timestamp with time zone,
+--    createdbyid integer constraint fk_mantra_postedbyid references engine.__member(id) on update cascade on delete set null,
+--    dateupdated timestamp with time zone,
+--    updatedbyid integer constraint fk_mantra_modifiedbyid references engine.__member(id) on update cascade on delete set null
 );
 
 create view engine.mantra as
     select 
         *,
-        extract(epoch from datecreated) as datecreatedepoch,
-        extract(epoch from dateupdated) as dateupdatedepoch
-    from engine.__mantra;
+        (attributes->>'description') as description,
+        (attributes->>'author') as author,
+        (attributes->>'reference') as reference
+    from engine.node
+;
 
 grant select on engine.mantra to apache;
-grant insert, update, delete on engine.__mantra to apache;
+--grant insert, update, delete on engine.__mantra to apache;
