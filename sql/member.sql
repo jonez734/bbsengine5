08286962817b68ebef609c5e45f8931ab29b31b3
Jeff MacDonald <jam@zoidtechnologies.com>
2021-01-28 19:31:00 -0500

    bbsengine5/sql/member.sql: added 'notifycount' and 'sentnotifycount' to engine.member; removed 'username' field, marked 'name' field 'unique not null'.

--- sql/member.sql
+++ sql/member.sql
@@ -2,8 +2,7 @@
 
 create table engine.__member (
   "id" bigserial unique not null primary key,
-  "username" text unique not null,
-  "name" text,
+  "name" text unique not null,
   "email" text,
   "password" text,
   "datecreated" timestamptz,
@@ -20,13 +19,14 @@ create table engine.__member (
 grant all on engine.__member to apache;
 grant all on engine.__member_id_seq to apache;
 
-create view engine.member as
+create or replace view engine.member as
   select m.*,
-  extract(epoch from datecreated) as datecreatedepoch,
-  extract(epoch from lastlogin) as lastloginepoch,
-  extract(epoch from dateapproved) as dateapprovedepoch,
-  extract(epoch from dateupdated) as dateupdatedepoch
-
+  (select count(notify1.id) from engine.notify as notify1 where notify1.memberid = m.id) as notifycount,
+  (select count(notify2.id) from engine.notify as notify2 where notify2.memberid = m.id) and notify2.status ='sent') as sentnotifycount
+  extract(epoch from m.datecreated) as datecreatedepoch,
+  extract(epoch from m.lastlogin) as lastloginepoch,
+  extract(epoch from m.dateapproved) as dateapprovedepoch,
+  extract(epoch from m.dateupdated) as dateupdatedepoch
   from engine.__member as m
 ;
 
