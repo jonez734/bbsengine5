83bf70f3ae0ab95d47f926c6446e9ab103163d34
Jeff MacDonald <jam@zoidtechnologies.com>
2021-01-30 19:28:01 -0500

    bbsengine5/sql/node.sql: re-added proper constraints to engine.map_node_sig; in view engine.node, replaced 'username' with 'name'; changed whitespace of the left joins.

--- sql/node.sql
+++ sql/node.sql
@@ -19,8 +19,8 @@ grant insert, update, delete on engine.__node to apache;
 create index idx_node_attributes ON engine.__node USING GIN (attributes);
 
 create table if not exists engine.map_node_sig (
-    "nodeid" bigint, -- constraint fk_engine_map_node_sig_nodeid, -- references engine.__node(id) on update cascade on delete cascade,
-    "sigpath" ltree -- constraint fk_engine_map_node_sig_sigpath -- references engine.__node(attributes->>'path') on update cascade on delete cascade
+    "nodeid" bigint constraint fk_engine_map_node_sig_nodeid references engine.__node(id) on update cascade on delete cascade,
+    "sigpath" ltree constraint fk_engine_map_node_sig_sigpath references engine.__sig(path) on update cascade on delete cascade
 );
 
 grant insert, update, delete, select on engine.map_node_sig to apache;
@@ -32,14 +32,15 @@ create or replace view engine.node as
         extract(epoch from n.datecreated) as datecreatedepoch,
         extract(epoch from n.dateupdated) as dateupdatedepoch,
         extract(epoch from n.dateapproved) as dateapprovedepoch,
-        coalesce(m1.username, 'a. nonymous'::text) as createdbyname,
-        coalesce(m2.username, 'a. nonymous'::text) as updatedbyname,
-        coalesce(m3.username, 'a. nonymous'::text) as approvedbyname,
-        array(select distinct map.sigpath from engine.map_node_sig as map where map.nodeid = n.id order by map.sigpath) AS sigs
+        coalesce(m1.name, 'a. nonymous'::text) as createdbyname,
+        coalesce(m2.name, 'a. nonymous'::text) as updatedbyname,
+        coalesce(m3.name, 'a. nonymous'::text) as approvedbyname,
+        array(select distinct map.sigpath from engine.map_node_sig as map where map.nodeid = n.id order by map.sigpath) AS sigs,
+        (select count(id) from (select id from engine.__node as subnode where subnode.parentid=n.id) as subnodes) as subnodecount
     from engine.__node as n
-left join engine.__member as m1 ON (m1.id = n.createdbyid)
-left join engine.__member as m2 ON (m2.id = n.updatedbyid)
-left join engine.__member as m3 ON (m3.id = n.approvedbyid)
+    left join engine.__member as m1 ON (m1.id = n.createdbyid)
+    left join engine.__member as m2 ON (m2.id = n.updatedbyid)
+    left join engine.__member as m3 ON (m3.id = n.approvedbyid)
 ;
 
 grant select on engine.node to apache;
