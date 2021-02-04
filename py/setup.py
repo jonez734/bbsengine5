02927bdb8e3af1b1c38ed8f440258c941e7f48be
Jeff MacDonald <jam@zoidtechnologies.com>
2021-02-03 19:51:17 -0500

    bbsengine5/py/setup.py: copied from bbsengine4/py/setup.py and customized: depend on ttyio4 instead of 3, changed 'bbsengine4' to 'bbsengine5'.

--- py/setup.py
+++ py/setup.py
@@ -5,16 +5,16 @@ from distutils.core import setup
 import time
 
 v = time.strftime("%Y%m%d%H%M")
-projectname = "bbsengine4"
+projectname = "bbsengine5"
 
 setup(
   name=projectname,
   version=v,
   author="zoid technologies",
   author_email="%s@projects.zoidtechnologies.com" % (projectname),
-  py_modules=["bbsengine4"],
+  py_modules=["bbsengine5"],
   scripts=[],
-  requires=["ttyio3"],
+  requires=["ttyio4"],
   url="http://bbsengine.org/",
   classifiers=[
     "Programming Language :: Python :: 3.7",
