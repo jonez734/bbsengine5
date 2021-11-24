8c6f00200f65a11b1ae164dbf66043870dd860c5
Jeff MacDonald <jam@zoidtechnologies.com>
2021-11-23 23:22:44 -0500

    - bbsengine5/py/testareastack.py: added.

---
+++ py/testareastack.py
@@ -0,0 +1,55 @@
+import ttyio5 as ttyio
+import bbsengine5 as bbsengine
+
+def poparea():
+
+  ttyio.echo("poparea.120: areastack=%r" % (bbsengine.areastack), interpret=False)
+  if len(bbsengine.areastack) < 1:
+    return
+
+  width = ttyio.getterminalwidth()
+
+  bbsengine.areastack.pop()
+  if len(bbsengine.areastack) > 0:
+    buf = bbsengine.areastack[-1]
+    ttyio.echo("poparea.100: areastack=%r buf=%r" % (bbsengine.areastack, buf), interpret=False)
+    bbsengine.updatebottombar("{var:engine.areacolor} %s {/all}" % (buf.ljust(width-2, " ")))
+  ttyio.echo("poparea.140: areastack=%r" % (bbsengine.areastack), interpret=False)
+  return
+
+def main():
+  ttyio.setvariable("engine.areacolor", "{bggray}{white}")
+  ttyio.echo("{f6:3}{curpos:%d,0}" % (ttyio.getterminalheight()-3))
+  bbsengine.initscreen(bottommargin=1)
+  bbsengine.setarea("test alpha")
+  ttyio.inputboolean("continue: ", "Y")
+  bbsengine.setarea("test bravo")
+  ttyio.inputboolean("continue: ", "Y")
+  bbsengine.setarea("test charlie")
+  ttyio.inputboolean("continue: ", "Y")
+
+  ttyio.echo(bbsengine.hr())
+
+  ttyio.echo("areastack=%r" % (bbsengine.areastack), interpret=False)
+
+  poparea()
+  ttyio.echo("areastack=%r" % (bbsengine.areastack), interpret=False)
+  ttyio.inputboolean("continue: ", "Y")
+
+  poparea()
+  ttyio.echo("areastack=%r" % (bbsengine.areastack), interpret=False)
+  ttyio.inputboolean("continue: ", "Y")
+
+  poparea()
+  ttyio.echo("areastack=%r" % (bbsengine.areastack), interpret=False)
+  ttyio.inputboolean("continue: ", "Y")
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        ttyio.echo("{/all}{bold}INTR{bold}")
+    except EOFError:
+        ttyio.echo("{/all}{bold}EOF{/bold}")
+    finally:
+        ttyio.echo("{decsc}{curpos:%d,0}{el}{decrc}{reset}{/all}" % (ttyio.getterminalheight()))
