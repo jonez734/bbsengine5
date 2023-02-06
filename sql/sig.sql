65f71e24b9dfb6f094811876cf32d8242dd627b0
Jeff MacDonald <jam@zoidtechnologies.com>
2023-02-06 14:54:02 -0500

    - bbsengine5/sql/sig.sql: removed call to buildsiguri() (pl/pythonu)

--- sql/sig.sql
+++ sql/sig.sql
@@ -1,20 +1,23 @@
 \echo sig
 create table engine.__sig (
-    id serial not null primary key,
-    intro text,
-    path ltree unique not null,
-    title text,
-    attributes jsonb,
-    dateupdated timestamptz,
-    updatedbyid integer constraint fk_engine_sig_updatedbyid references engine.__member(id) on update cascade on delete set null,
-    datecreated timestamptz,
-    createdbyid integer constraint fk_engine_sig_createdbyid references engine.__member(id) on update cascade on delete set null
+--    id bigserial not null primary key,
+    "path" ltree unique not null primary key,
+    "uri"  text unique,
+    "title" text,
+    "name" text,
+    "intro" text,
+    "attributes" jsonb,
+    "dateupdated" timestamptz,
+    "updatedbyid" bigint constraint fk_engine_sig_updatedbyid references engine.__member(id) on update cascade on delete set null,
+    "dateapproved" timestamptz,
+    "approvedbyid" bigint constraint fk_engine_sig_approvedbyid references engine.__member(id) on update cascade on delete set null,
+    "datecreated" timestamptz,
+    "createdbyid" bigint constraint fk_engine_sig_createdbyid references engine.__member(id) on update cascade on delete set null
 );
 
 create view engine.sig as
  select 
   s.*,
-  engine.buildsiguri(path) as uri,
   coalesce(m1.name, 'a. nonymous'::text) as createdbyname,
   coalesce(m2.name, 'a. nonymous'::text) as updatedbyname
  from engine.__sig as s
@@ -22,9 +25,9 @@ create view engine.sig as
  left join engine.__member as m2 ON (m2.id = s.updatedbyid)
 ;
 
-create unique index idx_engine_sig_path on engine.__sig(path);
+--create unique index idx_engine_sig_path on engine.__sig(path);
 
 grant select on engine.sig to apache;
 
 grant all on table engine.__sig to apache;
-grant all on sequence engine.__sig_id_seq to apache;
+--grant all on sequence engine.__sig_id_seq to apache;
